56a32f4ca2a88cdd2b97a9c21d8be8064fea35fc
Kvantin17 <tvantin17@gmail.com>
2024-07-26 23:09:39 +0300

    settings page

---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createEditCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+
+export function useUpdateSetting() {
+  const queryClient = useQueryClient();
+
+  // Редактирование элемента
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      toast.success("Settings succesfully edited");
+      // новый запрос данных после добавления
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (err) => toast.error(err.messages),
+  });
+
+  return { isUpdating, updateSetting };
+}
