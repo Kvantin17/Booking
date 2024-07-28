728bca4227c79737a3304c507eb839f78d044282
Kvantin17 <tvantin17@gmail.com>
2024-07-28 23:36:21 +0300

    sorting

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -27,6 +27,7 @@ const CabinTable = () => {
 
   if (isLoading) return <Spinner />;
 
+  // Фильтр
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -35,6 +36,14 @@ const CabinTable = () => {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // Сортировка
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -51,7 +60,7 @@ const CabinTable = () => {
         {console.log(cabins)}
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
