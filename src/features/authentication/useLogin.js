9f84abd0be62d44668c8e1676089dc00ab966c1e
Kvantin17 <tvantin17@gmail.com>
2024-07-30 22:32:07 +0300

    logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -12,9 +12,9 @@ export function useLogin() {
     onSuccess: (user) => {
       // благодаря setQueriesData мы можем засовывать вручную те или иные данные в кэш React Query
       // В данном случае мы засовывваем в кэш 'user' данные чтоб лишний раз не сработал запрос в хуке useUser
-      queryClient.setQueriesData(["user"], user);
-
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user.user);
+      // replace нужен чтоб нельзя было вернуться на страницу нажимая кнопку назад в браузере
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
       console.log("error", err);
