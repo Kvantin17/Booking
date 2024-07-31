75cf583f4b1d5cf90bcc5dc4ba1c1aa9eb0a886e
Kvantin17 <tvantin17@gmail.com>
2024-07-31 11:53:23 +0300

    Sign Up

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,13 @@
+import SignupForm from "../features/authentication/SignupForm";
+import Heading from "../ui/Heading";
+
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <>
+      <Heading as="h1">Users</Heading>
+      <SignupForm />
+    </>
+  );
 };
 
 export default Users;
